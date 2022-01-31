5e37778070d05c1b6b20d9f1d57d9bde79f5f866
davidjinli <61616862+davidjinli@users.noreply.github.com>
2022-01-31 17:48:00 +0000

    Adding basic model for divisions and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by David Jin Li on 31/01/2022.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
---
+++ AbsenceRecorder/Model/Student.swift
@@ -0,0 +1,20 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by David Jin Li on 31/01/2022.
+//
+
+import Foundation
+
+class Student {
+    let forename: String
+    let surname: String
+    let birthday: Date
+    
+    init(forename: String, surname: String, birthday: Date) {
+        self.forename = forename
+        self.surname = surname
+        self.birthday = birthday
+    }
+}
